67781e4ea147d111cca2fb15bf45977433932289
ccharlieee <812368191@qq.com>
2023-02-25 22:37:36 +0800

    a nice new port kernel

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Sat Feb 25 20:06:53 CST 2023"
+#define LINUX_COMPILE_BY "edu"
+#define LINUX_COMPILE_HOST "edu"
+#define LINUX_COMPILER "gcc version 4.9.4 (Linaro GCC 4.9-2017.01) "
