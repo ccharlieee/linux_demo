1e111c3ba0485ef23ba9410e2d2567deffd31caf
ccharlieee <812368191@qq.com>
2023-05-23 17:01:39 +0800

    modity app.c

--- linux_test/led_test/app.c
+++ linux_test/led_test/app.c
@@ -3,13 +3,21 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(int argc, char const *argv[])
 {
-    int fd = open("dev/led_test" ,O_RDWR);
+    int fd = open("/dev/demo_dev.txt" ,O_RDWR);
+    if(fd < 0)
+	{
+		perror("/dev/demo_dev:");
+		return -1;
+	}
     char buf[100]= "0";
+    printf("in app\n");
     while(1)
     {
+        printf("in while");
         buf[0] = '0';
         write(fd, buf, strlen(buf)+1);
         sleep(1);
